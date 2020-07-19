56c6682b02bdada9b9951194589eb6247afae689
Theingi74 <68505370+Theingi74@users.noreply.github.com>
2020-07-20 04:05:47 +0900

    Update email.php

--- email.php
+++ email.php
@@ -1,10 +1,3 @@
 <?php
 echo "Sending Email 
-echo "Another email sent"
-echo "third line from Terminal"
-echo "Third line from online"
-
-
-echo "Fourth Line From Online"
-echo "fourth line from Terminal"
 
